2aeab264bc295f71f45fd2a1efd2d2d6d4f0e816
Kushal <kushal_pandya96@hotmail.com>
2017-02-17 22:15:20 -0500

    got some things working for command line viewer

---
+++ README.md
@@ -0,0 +1,26 @@
+
+
+WHAT DOESNT WORK:
+----------------------------
+
+post:
+	author name more than 1 word
+
+
+addAuthor:
+	author name more than 1 word
+
+
+view:
+	- author name more than 1 word
+	
+	functions:
+		- pageup
+		- pagedown
+		- ordertoggle
+		- markall
+		- checkfornew
+
+	- 'all' streams
+
+	
--- view.py
+++ view.py
@@ -3,7 +3,11 @@
 import sys
 import os
 import glob
-import fileinput
+
+import tty
+import sys
+import termios
+
 
 
 def getName():
@@ -15,7 +19,7 @@ def clearScreen():
 	os.system('cls' if os.name == 'nt' else 'clear')
 
 def printFooter():
-	print 'Up Arrow   Down Arrow   O-order toggle   M-mark all   S-stream   C-check for new'
+	print '+PageUp  -PageDown  o-orderToggle  m-markAll  s-newStream  c-checkForNew  q-quit'
 
 # May need to change the way the name is stored (change tokenize to delimiter rather than space)
 def getListOfStreams(name):
@@ -39,7 +43,7 @@ def getLastSeenPost(stream, name):
 	with open(filename) as f:
 		for line in f.readlines():
 			if line.strip().split(' ', 1)[0] == name:
-				return line.strip().split(' ', 1)[1]
+				return int(line.strip().split(' ', 1)[1])
 
 
 # See if this function is useful
@@ -58,9 +62,30 @@ def getPostPositions(stream):
 	return positions
 
 
+def selectStream(name):
+	# getting and printing list of streams
+	streamList = getListOfStreams(name)
+	if len(streamList) == 2:
+		print '%s does not exist in any streams.. exiting' % name
+		sys.exit()
+
+	for stream in streamList:
+		print stream,
+
+	stream = raw_input()
+	if stream not in streamList:
+		print 'Invalid stream'
+		sys.exit()
+
+	return stream
+
+
 def streamSelected(stream, name):
 	filename = 'messages/%sStream' % stream
-	lastSeenIndex = int(getLastSeenPost(stream, name))
+	lastSeenIndex = getLastSeenPost(stream, name)
+	lineCount = 0
+	postRead = 0
+	check = 0
 
 	if os.path.isfile(filename) == False:
 		print '%s does not exist.. exiting' % filename
@@ -70,13 +95,59 @@ def streamSelected(stream, name):
 		f.seek(lastSeenIndex, 0)
 
 		positions = getPostPositions(stream)
-		del positions[-1]
-		for i in range(23):
-			if f.tell() in positions:
-				print '-'*50
+
+		while (lineCount < 22):
+			if f.tell() in positions: 
+
+				if f.tell() == positions[-1]:
+					check = 1
+				if check == 0:
+					print '-'*50
+				lineCount = lineCount + 1
+				postRead = f.tell()
+
 			print f.readline(),
-		updateUser(stream, name, f.tell())
+			lineCount = lineCount + 1
+
+		if check == 1:
+			print '\nUser has read all posts, press the Up Arrow to view older posts'
+
+		updateUser(stream, name, postRead)
 		print '\n'
+
+
+def keyPressed(stream, name):
+	orig_settings = termios.tcgetattr(sys.stdin)
+
+	tty.setraw(sys.stdin)
+	x = 0
+
+	while x != 'q': 
+		x = sys.stdin.read(1)[0]
+		clearScreen()
+		
+		if x == '+':
+			print 'PAGE UP'
+		elif x == '-':
+			print 'PAGE DOWN'
+		elif x == 'o':
+			print 'ORDER TOGGLE'
+		elif x == 'm':
+			print 'MARK ALL'
+		elif x == 's':
+			termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
+			stream = selectStream(name)
+			clearScreen()
+			streamSelected(stream, name)
+			printFooter()
+			tty.setraw(sys.stdin)
+		elif x == 'c':
+			print 'CHECK FOR NEW'
+		elif x == 'q':
+			termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings) 
+			sys.exit()
+
+	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)  
 
 
 def updateUser(stream, name, newRead):
@@ -95,26 +166,13 @@ def updateUser(stream, name, newRead):
 
 if __name__ == "__main__":
 	name = getName()
-
-	# getting and printing list of streams
-	streamList = getListOfStreams(name)
-	if len(streamList) == 2:
-		print '%s does not exist in any streams.. exiting' % name
-		sys.exit()
-
-	for stream in streamList:
-		print stream,
-
-	stream = raw_input()
-	if stream not in streamList:
-		print 'Invalid stream'
-		sys.exit()
+	stream = selectStream(name)
 
 	clearScreen()
 	streamSelected(stream, name)
-
-	# get user input here
-
-
 	printFooter()
+
+	keyPressed(stream, name)
+
+
 
